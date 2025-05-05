eefe4553ea7bde7cf4af9fe5df02d6da9fef7af7
Abaan Nazar <abaannazar3@gmail.com>
2025-05-05 12:11:29 +0530

    Upadated frontend

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,16 +1,24 @@
+
 // models/User.js , schema created
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String, 
+    required: true 
+  },
+
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true 
   },
-  password: {
-    type: String,
-    required: true
-  }
-}, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema);
+  password: { 
+    type: String,
+    required: true 
+  },
+
+});
+
+module.exports = mongoose.model("User", userSchema);
