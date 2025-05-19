a9ede73e99996611fd26d6cd37f5cbd3c6a91e94
Abaan2006 <84410795+abaannazar@users.noreply.github.com>
2025-05-19 15:42:08 +0530

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import MakeDesign from './pages/MakeDesign';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-     
-    </>
+    <Home/>
   )
 }
 
---
+++ frontend/src/pages/MakeDesign.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MakeDesign() {
+  return (
+    <div>MakeDesign</div>
+  )
+}
+
+export default MakeDesign
